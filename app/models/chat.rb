f53d3dd5aa65336f6eb23e97345aae8f2e0e1d60
manu raffard <manu_raffard@hotmail.com>
2026-03-16 17:54:21 +0100

    chat finis

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -4,4 +4,7 @@ class Chat < ApplicationRecord
 
   has_many :messages, dependent: :destroy
   validates :title, presence: true, length: { minimum: 3 }
+  def user_messages_count
+    messages.where(role: "user").count
+  end
 end
